702a38547763eb237049ad8279de6e4d7ba6db5c
xiaojuquan <xiaojuquan@163.com>
2017-12-04 18:02:06 +0800

    gavin

--- a.cpp
+++ a.cpp
@@ -1,35 +1,21 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdarg.h>
 
-using namespace std;
 
-class Singleton  
-{  
- public:  
-    static Singleton& GetInstance()  
-    {  
-        static Singleton instance_;                   
-        return instance_;  
-    }  
-  
-    ~Singleton()  
-    {  
-        cout << "~Singleton"<<endl;  
-    }  
-    
-private:  
-  
-    Singleton()  
-    {  
-        cout << "Singleton "<<endl;  
-    }  
-    Singleton(Singleton const &other);  
-    Singleton & operator=(const Singleton &other);  
-};  
+int log(char *s, int size, const char *fmt, ...) //该自定义函数，与系统提供的snprintf()函数相同。
+{
+    va_list ap;
+    int n = 0;
+    va_start(ap, fmt); //获得可变参数列表,分析fmt，得出各个参数的类型，也是分析参数中类似”%d%s“的字符串
+    n = vsnprintf(s, size, fmt, ap); //写入字符串s
+    va_end(ap); //释放资源,必须与va_start成对调用
+    return n; //返回写入的字符个数
+}
 
-int main()  
-{  
-    Singleton &s1 = Singleton::GetInstance();  
-    Singleton &s2 = Singleton::GetInstance(); //s1与s2是同一对象的引用  
-  
-    return 0;  
-}  
+int main()
+{
+    char s[1000];
+    log(s, 1000, "im ok:%d %j\n", 1000, "abc");
+    printf("%s", s);
+    return 0;
+}
